f8985b57b3fc7a2af306a06e0feb02e6fbf4abe0
Gurdip Singh <singh.gurdip24@gmail.com>
2019-02-20 11:31:56 -0500

    Cloned for learning Purpose

---
+++ app.js
@@ -0,0 +1,33 @@
+var form = $("#form");
+var reset = $("#reset");
+var brush = $(".brush");
+var input = $("#color-field");
+var paint = true
+var $square;
+var eraser = $("#eraser")
+
+
+//as long as I is less that 8094 add a div with class name square
+for(var i = 0; i < 9500; i++){
+   $square = $("<div class='square'/>");
+  $("body").append($square);
+}
+//on mouseover change square background color to the input value
+$(".square").on("mouseover", function(){
+    if(paint === true){
+        $(this).css('background', input.val())
+    }
+})
+
+//On click turn background squares back to white
+eraser.on("click", function(){
+    $(".square").css('background-color', 'white')
+})
+
+$(document).keydown(function(e){
+    paint == true ? paint = false : paint = true
+})
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
